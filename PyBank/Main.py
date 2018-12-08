c4c7f8c21a9e89bf4494bae592da5982a74d34f6
obrien630 <obrien630@gmail.com>
2018-12-08 16:47:35 -0500

    Adding new PyBank and PyPoll Main.py files

---
+++ PyBank/Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 16:38:40 2018
+
+@author: obrie
+"""
+
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  8 15:46:46 2018
+Created on Sat Dec  8 16:37:39 2018
 
 @author: obrie
 """
